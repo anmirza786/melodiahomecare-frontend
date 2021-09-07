cccdb60f6a4805aa67f906d13412126d36f19c02
anmirza786 <anmirza360@gmail.com>
2021-09-07 15:40:27 +0500

    update 2.1.

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ let current = 1;
 
 nextBtnFirst.addEventListener("click", function(event){
   event.preventDefault();
-  slidePage.style.display = "none";
+  slidePage.style.marginLeft = "-25%";
   page_2nd.style.display = "block";
   bullet[current - 1].classList.add("active");
   progressCheck[current - 1].classList.add("active");
@@ -26,7 +26,7 @@ nextBtnFirst.addEventListener("click", function(event){
 });
 nextBtnSec.addEventListener("click", function(event){
   event.preventDefault();
-  page_2nd.style.display = "none";
+  slidePage.style.marginLeft = "-50%"
   page_3rd.style.display = "block";
   bullet[current - 1].classList.add("active");
   progressCheck[current - 1].classList.add("active");
@@ -35,7 +35,7 @@ nextBtnSec.addEventListener("click", function(event){
 });
 nextBtnThird.addEventListener("click", function(event){
   event.preventDefault();
-  page_3rd.style.display = "none";
+  slidePage.style.marginLeft = "-75%"
   page_4th.style.display = "block"
   bullet[current - 1].classList.add("active");
   progressCheck[current - 1].classList.add("active");
@@ -47,16 +47,13 @@ submitBtn.addEventListener("click", function(){
   progressCheck[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
   current += 1;
-  setTimeout(function(){
-    alert("Your Application is Successfully Submitted");
-    location.reload();
-  },800);
 });
 
 prevBtnSec.addEventListener("click", function(event){
   event.preventDefault();
+  slidePage.style.marginLeft = "0%"
   slidePage.style.display = "block";
-  page_2nd.style.display = "none";
+  
   bullet[current - 2].classList.remove("active");
   progressCheck[current - 2].classList.remove("active");
   progressText[current - 2].classList.remove("active");
@@ -64,8 +61,9 @@ prevBtnSec.addEventListener("click", function(event){
 });
 prevBtnThird.addEventListener("click", function(event){
   event.preventDefault();
+  slidePage.style.marginLeft = "-25%"
   page_2nd.style.display = "block";
-  page_3rd.style.display = "none";
+  
   bullet[current - 2].classList.remove("active");
   progressCheck[current - 2].classList.remove("active");
   progressText[current - 2].classList.remove("active");
@@ -73,8 +71,9 @@ prevBtnThird.addEventListener("click", function(event){
 });
 prevBtnFourth.addEventListener("click", function(event){
   event.preventDefault();
+  slidePage.style.marginLeft = "-50%"
   page_3rd.style.display = "block";
-  page_4th.style.display = "none";
+  
   bullet[current - 2].classList.remove("active");
   progressCheck[current - 2].classList.remove("active");
   progressText[current - 2].classList.remove("active");
